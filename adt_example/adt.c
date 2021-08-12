b13bccd23e875b85f09e89667b125ebbd4f9a93f
Takumi Crary <tcrary@gmail.com>
2021-08-12 22:31:12 +0900

    Finish adt.c example

--- adt_example/adt.c
+++ adt_example/adt.c
@@ -1,21 +1,35 @@
 #ifndef ADT_H
 #define ADT_H
+#include <stdio.h>
 
-typedef struct tree_struct {
+enum TREE_TAG {
+    NODE,
+    LEAF
+};
+
+struct Tree {
+    enum TREE_TAG tag;
     union {
-
-    }
-} tree;
+        struct Node {
+            struct Tree * left;
+            struct Tree * right;
+        } node;
+        int value;
+    };
+} Tree;
 
 int main(void){
+    struct Tree tx = ((struct Tree) {
+        .tag = NODE, .node = ((struct Node){
+            &(struct Tree){ .tag = LEAF, .value = 5},
+            &(struct Tree){ .tag = LEAF, .value = 6}
+        })
+    });
 
-    // Declaring a tree to inspect
-
-
+    printf("%d\n", tx.node.left->value);
+    printf("%d\n", tx.node.right->value);
 
     return 0;
 }
-
-
 
 #endif
